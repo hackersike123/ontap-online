f062d3ad571a38e58da0429c0c0ba09feaa11f7b
hackersike123 <dungcoquen1000@gmail.com>
2025-06-30 13:41:05 +0700

    update 2

--- script.js
+++ script.js
@@ -100,16 +100,20 @@ function showSummary(score, userAnswers) {
   userAnswers.forEach((ans, idx) => {
     html += `<div class='answer-review' style='margin-bottom:16px;padding:10px 0;border-bottom:1px solid #eee'>`;
     html += `<b>Câu ${idx+1}:</b> ${ans.question}<br>`;
-    html += `<span>Đáp án bạn chọn: <b style='color:${ans.selected === ans.correct ? '#228B22':'#d00'}'>${ans.selected ? ans.selected : 'Không chọn'}</b>`;
-    if(ans.selectedText) {
-      html += ` - ${ans.selectedText}`;
+    html += `<span>Đáp án bạn chọn: <b style='color:${ans.selected === ans.correct ? '#228B22':'#d00'}'>`;
+    if(ans.selected) {
+      html += `${ans.selected} - ${ans.selectedText}`;
+    } else {
+      html += 'Không chọn';
     }
-    html += `</span><br>`;
-    html += `<span>Đáp án đúng: <b style='color:#228B22'>${ans.correct ? ans.correct : 'Không xác định'}</b>`;
-    if(ans.correctText) {
-      html += ` - ${ans.correctText}`;
+    html += `</b></span><br>`;
+    html += `<span>Đáp án đúng: <b style='color:#228B22'>`;
+    if(ans.correct) {
+      html += `${ans.correct} - ${ans.correctText}`;
+    } else {
+      html += 'Không xác định';
     }
-    html += `</span>`;
+    html += `</b></span>`;
     html += '</div>';
   });
   html += '</div>';
